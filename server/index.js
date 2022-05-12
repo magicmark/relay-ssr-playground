119941c8a1e1ebe450976deb116ca6f054be37f8
Mark Larah <markl@yelp.com>
2022-05-12 11:42:46 -0400

    fix path

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3010;
 
-app.use(express.static(path.join(__dirname, "dist")));
+const DIST = path.join(__dirname, "..", "dist");
+
+app.use(express.static(DIST));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(DIST, "index.html"));
 });
 
 app.listen(port, () => {
